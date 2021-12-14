04225a3aff4627f2daa28a8b03a4b29f22653f7d
Илья <ilia.volkov2016@mail.ru>
2021-12-14 18:32:49 +0300

    Коммит 12 урока

--- app/src/main/java/ru/iliavolkov/notes/ui/ShowFragNote.java
+++ app/src/main/java/ru/iliavolkov/notes/ui/ShowFragNote.java
@@ -116,7 +116,60 @@ public class ShowFragNote extends Fragment {
 
 //
 
+    private void saveNote() {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getContext());
+        SharedPreferences.Editor editor = prefs.edit();
+        Gson gson = new Gson();
 
+        TextView title = requireActivity().findViewById(R.id.title);
+        TextView description = requireActivity().findViewById(R.id.description);
+
+        Date current = new Date();
+        SimpleDateFormat formatter = new SimpleDateFormat("dd MMM");
+        String message = formatter.format(current);
+        if (title != null || description != null) {
+            NotesData notes = new NotesData(title.getText().toString(), description.getText().toString(), message);
+
+            String json = prefs.getString("arrNotes", null);
+            Type type = new TypeToken<ArrayList<NotesData>>() {
+            }.getType();
+
+            ArrayList<NotesData> arrNotes;
+            if (json == null) arrNotes = new ArrayList<>();
+            else arrNotes = gson.fromJson(json, type);
+
+            if (MainActivity.floatingBtnBool) {
+                if (!title.getText().toString().equals("") || !description.getText().toString().equals(""))
+                    arrNotes.add(0, notes);
+            } else if (MainActivity.pressButtonKey) {
+                if (title.getText().toString().replace(" ", "").equals("") && description.getText().toString().replace(" ", "").equals(""))
+                    arrNotes.remove(indexGet);
+                else { //if (!titleGet.equals("") || !descriptionGet.equals(""))
+                    arrNotes.remove(indexGet);
+                    arrNotes.add(0, notes);
+                }
+                try {
+                    if (Utils.isLandscape(getResources())) {
+                        NotesFragmentListView notesFragmentListView = new NotesFragmentListView();
+                        FragmentManager fragmentManager = requireActivity().getSupportFragmentManager();
+                        FragmentTransaction transaction = fragmentManager.beginTransaction();
+                        transaction.replace(R.id.fragment_container, notesFragmentListView);
+                        transaction.commit();
+                    }
+                } catch (RuntimeException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            MainActivity.floatingBtnBool = false;
+            MainActivity.pressButtonKey = false;
+            json = gson.toJson(arrNotes);
+            editor.putString("arrNotes", json);
+            editor.apply();
+            title.setText(null);
+            description.setText(null);
+        }
+    }
 
     @Override
     public void onStop() {
